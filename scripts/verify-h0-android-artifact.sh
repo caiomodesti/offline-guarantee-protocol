80cfd4d1e1ca3b4ecbb702b8b3a1f73fef802764
Caio <caio.modesti7@gmail.com>
2026-08-17 22:57:51 -0300

    Disable Android backup for H0 payer

--- scripts/verify-h0-android-artifact.sh
+++ scripts/verify-h0-android-artifact.sh
@@ -13,27 +13,13 @@ test -n "$build_tools"
 "$build_tools/aapt" dump badging "$apk" | grep -E "^(package:|sdkVersion:|targetSdkVersion:|native-code:)"
 
 manifest="$("$build_tools/aapt" dump xmltree "$apk" AndroidManifest.xml)"
-grep -q 'A: android:allowBackup.*0xffffffff' <<<"$manifest"
+grep -q 'A: android:allowBackup.*0x0' <<<"$manifest"
 if grep -q 'A: android:debuggable.*0xffffffff' <<<"$manifest"; then
   echo 'H0 APK must not be debuggable' >&2
   exit 1
 fi
-
-apkanalyzer="$android_sdk/cmdline-tools/latest/bin/apkanalyzer"
-test -x "$apkanalyzer"
-backup_file="$("$apkanalyzer" resources value --config default --type xml --name secure_store_backup_rules --package protocol.ogp.payer "$apk")"
-extraction_file="$("$apkanalyzer" resources value --config default --type xml --name secure_store_data_extraction_rules --package protocol.ogp.payer "$apk")"
-backup_rules="$("$apkanalyzer" resources xml --file "$backup_file" "$apk")"
-extraction_rules="$("$apkanalyzer" resources xml --file "$extraction_file" "$apk")"
-
-test "$(grep -c '<include' <<<"$backup_rules")" -eq 1
-test "$(grep -c 'domain="sharedpref"' <<<"$backup_rules")" -eq 2
-grep -q 'path="SecureStore"' <<<"$backup_rules"
-test "$(grep -c '<include' <<<"$extraction_rules")" -eq 2
-test "$(grep -c 'domain="sharedpref"' <<<"$extraction_rules")" -eq 4
-test "$(grep -c 'path="SecureStore"' <<<"$extraction_rules")" -eq 2
-if grep -Eq 'domain="(database|file|root|external|device_database|device_file|device_root)"' <<<"$backup_rules$extraction_rules"; then
-  echo 'H0 backup policy unexpectedly includes application data outside shared preferences' >&2
+if grep -Eq 'A: android:(fullBackupContent|dataExtractionRules)' <<<"$manifest"; then
+  echo 'H0 payer must not reference Android backup or extraction rules' >&2
   exit 1
 fi
 
